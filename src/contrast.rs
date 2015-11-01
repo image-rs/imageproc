e313e8c07afc023cb22a6bea200a9ad52cf778fb
Phil Ellison <phil.j.ellison@gmail.com>
2015-11-01 12:34:34 +0000

    Run rustfmt on affine and contrast

--- src/contrast.rs
+++ src/contrast.rs
@@ -1,11 +1,6 @@
 //! Functions for manipulating the contrast of images.
 
-use image::{
-    GenericImage,
-    GrayImage,
-    ImageBuffer,
-    Luma
-};
+use image::{GenericImage, GrayImage, ImageBuffer, Luma};
 
 /// Returns the Otsu threshold level of an 8bpp image.
 /// This threshold will optimally binarize an image that
@@ -13,7 +8,8 @@ use image::{
 /// with equal variances. For details see:
 /// Xu, X., et al. Pattern recognition letters 32.7 (2011)
 pub fn otsu_level<I>(image: &I) -> u8
-    where I: GenericImage<Pixel=Luma<u8>> {
+    where I: GenericImage<Pixel = Luma<u8>>
+{
     let hist = histogram(image);
     let levels = hist.len();
     let mut histc = [0i32; 256];
@@ -24,8 +20,8 @@ pub fn otsu_level<I>(image: &I) -> u8
 
     for i in 1..levels {
         pixel_count += hist[i] as f64;
-        histc[i] = histc[i-1] + hist[i];
-        meanc[i] = meanc[i-1] + (hist[i] as f64) * (i as f64);
+        histc[i] = histc[i - 1] + hist[i];
+        meanc[i] = meanc[i - 1] + (hist[i] as f64) * (i as f64);
     }
 
     let mut sigma_max = -1f64;
@@ -45,8 +41,7 @@ pub fn otsu_level<I>(image: &I) -> u8
             if sigma > sigma_max {
                 otsu = i as f64;
                 sigma_max = sigma;
-            }
-            else {
+            } else {
                 otsu2 = i as f64;
             }
         }
@@ -57,8 +52,8 @@ pub fn otsu_level<I>(image: &I) -> u8
 /// Returns a binarized image from an input 8bpp grayscale image
 /// obtained by applying the given threshold.
 pub fn threshold<I>(image: &I, thresh: u8) -> GrayImage
-    where I: GenericImage<Pixel=Luma<u8>> {
-
+    where I: GenericImage<Pixel = Luma<u8>>
+{
     let mut out: GrayImage = ImageBuffer::new(image.width(), image.height());
     out.copy_from(image, 0, 0);
     threshold_mut(&mut out, thresh);
@@ -68,14 +63,13 @@ pub fn threshold<I>(image: &I, thresh: u8) -> GrayImage
 /// Mutates given image to form a binarized version produced by applying
 /// the given threshold.
 pub fn threshold_mut<I>(image: &mut I, thresh: u8)
-    where I: GenericImage<Pixel=Luma<u8>> {
-
+    where I: GenericImage<Pixel = Luma<u8>>
+{
     for y in 0..image.height() {
         for x in 0..image.width() {
             if image.get_pixel(x, y)[0] as u8 <= thresh {
                 image.put_pixel(x, y, Luma([0]));
-            }
-            else {
+            } else {
                 image.put_pixel(x, y, Luma([255]));
             }
         }
@@ -84,10 +78,10 @@ pub fn threshold_mut<I>(image: &mut I, thresh: u8)
 
 /// Returns the histogram of grayscale values in an 8pp
 /// grayscale image.
-pub fn histogram<I>(image: &I) -> [i32;256]
-    where I: GenericImage<Pixel=Luma<u8>> {
-
-    let mut hist = [0i32;256];
+pub fn histogram<I>(image: &I) -> [i32; 256]
+    where I: GenericImage<Pixel = Luma<u8>>
+{
+    let mut hist = [0i32; 256];
 
     for pix in image.pixels() {
         hist[pix.2[0] as usize] += 1;
@@ -99,9 +93,9 @@ pub fn histogram<I>(image: &I) -> [i32;256]
 
 /// Returns the cumulative histogram of grayscale values in an 8bpp
 /// grayscale image.
-pub fn cumulative_histogram<I>(image: &I) -> [i32;256]
-    where I: GenericImage<Pixel=Luma<u8>> {
-
+pub fn cumulative_histogram<I>(image: &I) -> [i32; 256]
+    where I: GenericImage<Pixel = Luma<u8>>
+{
     let mut hist = histogram(image);
 
     for i in 1..hist.len() {
@@ -114,8 +108,8 @@ pub fn cumulative_histogram<I>(image: &I) -> [i32;256]
 /// Equalises the histogram of an 8bpp grayscale image in place.
 /// https://en.wikipedia.org/wiki/Histogram_equalization
 pub fn equalize_histogram_mut<I>(image: &mut I)
-    where I: GenericImage<Pixel=Luma<u8>> {
-
+    where I: GenericImage<Pixel = Luma<u8>>
+{
     let hist = cumulative_histogram(image);
     let total = hist[255] as f32;
 
@@ -132,8 +126,8 @@ pub fn equalize_histogram_mut<I>(image: &mut I)
 /// Equalises the histogram of an 8bpp grayscale image.
 /// https://en.wikipedia.org/wiki/Histogram_equalization
 pub fn equalize_histogram<I>(image: &I) -> GrayImage
-    where I: GenericImage<Pixel=Luma<u8>> {
-
+    where I: GenericImage<Pixel = Luma<u8>>
+{
     let mut out: GrayImage = ImageBuffer::new(image.width(), image.height());
     out.copy_from(image, 0, 0);
     equalize_histogram_mut(&mut out);
@@ -143,23 +137,13 @@ pub fn equalize_histogram<I>(image: &I) -> GrayImage
 #[cfg(test)]
 mod test {
 
-    use super::{
-        cumulative_histogram,
-        equalize_histogram,
-        histogram,
-        otsu_level,
-        threshold
-    };
-    use utils::{
-        gray_bench_image
-    };
-    use image::{
-        GrayImage,
-        ImageBuffer
-    };
+    use super::{cumulative_histogram, equalize_histogram, histogram, otsu_level, threshold};
+    use utils::gray_bench_image;
+    use image::{GrayImage, ImageBuffer};
     use test;
 
     #[test]
+    #[cfg_attr(rustfmt, rustfmt_skip)] 
     fn test_cumulative_histogram() {
         let image: GrayImage = ImageBuffer::from_raw(5, 1, vec![
             1u8, 2u8, 3u8, 2u8, 1u8]).unwrap();
@@ -172,7 +156,9 @@ mod test {
         assert_eq!(hist[3], 5);
         assert!(hist.iter().skip(4).all(|x| *x == 5));
     }
+
     #[test]
+    #[cfg_attr(rustfmt, rustfmt_skip)]
     fn test_histogram(){
         let image: GrayImage = ImageBuffer::from_raw(5, 1, vec![
             1u8, 2u8, 3u8, 2u8, 1u8]).unwrap();
@@ -185,6 +171,7 @@ mod test {
         assert_eq!(hist[3], 1);
     }
     #[test]
+    #[cfg_attr(rustfmt, rustfmt_skip)]
     fn test_otsu_level(){
         let image: GrayImage = ImageBuffer::from_raw(26, 1,
             vec![0u8, 10u8, 20u8, 30u8, 40u8, 50u8, 60u8, 70u8,
@@ -195,6 +182,7 @@ mod test {
         assert_eq!(level, 125);
     }
     #[test]
+    #[cfg_attr(rustfmt, rustfmt_skip)]
     fn test_threshold(){
         let original: GrayImage = ImageBuffer::from_raw(26, 1,
             vec![0u8, 10u8, 20u8, 30u8, 40u8, 50u8, 60u8, 70u8,
@@ -208,7 +196,6 @@ mod test {
                 255u8,  255u8,  255u8,  255u8]).unwrap();
 
         let actual = threshold(&original, 125u8);
-
         assert_pixels_eq!(expected, actual);
     }
 
@@ -218,6 +205,6 @@ mod test {
         b.iter(|| {
             let equalized = equalize_histogram(&image);
             test::black_box(equalized);
-            });
+        });
     }
 }
