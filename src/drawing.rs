4ca02c718a4b5d0bdd34858d17fcf1d0324c53d8
Marcus Huderle <huderlem@gmail.com>
2017-01-22 13:48:50 -0800

    Implement draw_filled_ellipse(). Add regression and bench tests for it.

--- src/drawing.rs
+++ src/drawing.rs
@@ -301,13 +301,56 @@ pub fn draw_hollow_ellipse_mut<I>(image: &mut I, center: (i32, i32), width_radiu
         return;
     }
 
-    let draw_quad_pixels = |image: &mut I, x0: i32, y0: i32, x: i32, y: i32, color: I::Pixel| {
+    let draw_quad_pixels = |x0: i32, y0: i32, x: i32, y: i32| {
         draw_if_in_bounds(image, x0 + x, y0 + y, color);
         draw_if_in_bounds(image, x0 - x, y0 + y, color);
         draw_if_in_bounds(image, x0 + x, y0 - y, color);
         draw_if_in_bounds(image, x0 - x, y0 - y, color);
     };
 
+    draw_ellipse(draw_quad_pixels, center, width_radius, height_radius);
+}
+
+/// Draw as much of an ellipse, including its contents, as lies inside the image bounds.
+pub fn draw_filled_ellipse<I>(image: &I,
+                             center: (i32, i32),
+                             width_radius: i32,
+                             height_radius: i32,
+                             color: I::Pixel) -> VecBuffer<I::Pixel>
+    where I: GenericImage,
+          I::Pixel: 'static
+{
+    let mut out = ImageBuffer::new(image.width(), image.height());
+    out.copy_from(image, 0, 0);
+    draw_filled_ellipse_mut(&mut out, center, width_radius, height_radius, color);
+    out
+}
+
+/// Draw as much of an ellipse, including its contents, as lies inside the image bounds.
+/// Uses Midpoint Ellipse Drawing Algorithm. (Modified from Bresenham's algorithm) (http://tutsheap.com/c/mid-point-ellipse-drawing-algorithm/)
+pub fn draw_filled_ellipse_mut<I>(image: &mut I, center: (i32, i32), width_radius: i32, height_radius: i32, color: I::Pixel)
+    where I: GenericImage,
+          I::Pixel: 'static
+{
+    // Circle drawing algorithm is faster, so use it if the given ellipse is actually a circle.
+    if width_radius == height_radius {
+        draw_filled_circle_mut(image, center, width_radius, color);
+        return;
+    }
+
+    let draw_line_pairs = |x0: i32, y0: i32, x: i32, y: i32| {
+        draw_line_segment_mut(image, ((x0 - x) as f32, (y0 + y) as f32), ((x0 + x) as f32, (y0 + y) as f32), color);
+        draw_line_segment_mut(image, ((x0 - x) as f32, (y0 - y) as f32), ((x0 + x) as f32, (y0 - y) as f32), color);
+    };
+
+    draw_ellipse(draw_line_pairs, center, width_radius, height_radius);
+}
+
+// Implements the Midpoint Ellipse Drawing Algorithm. (Modified from Bresenham's algorithm) (http://tutsheap.com/c/mid-point-ellipse-drawing-algorithm/)
+// Takes a function that determines how to render the points on the ellipse.
+fn draw_ellipse<F>(mut render_func: F, center: (i32, i32), width_radius: i32, height_radius: i32)
+    where F: FnMut(i32, i32, i32, i32)
+{
     let (x0, y0) = center;
     let w2 = width_radius * width_radius;
     let h2 = height_radius * height_radius;
@@ -316,7 +359,7 @@ pub fn draw_hollow_ellipse_mut<I>(image: &mut I, center: (i32, i32), width_radiu
     let mut px = 0;
     let mut py = 2 * w2 * y;
 
-    draw_quad_pixels(image, x0, y0, x, y, color);
+    render_func(x0, y0, x, y);
 
     // Top and bottom regions.
     let mut p = (h2 - (w2 * height_radius)) as f32 + (0.25 * w2 as f32);
@@ -331,7 +374,7 @@ pub fn draw_hollow_ellipse_mut<I>(image: &mut I, center: (i32, i32), width_radiu
             p += (h2 + px - py) as f32;
         }
 
-        draw_quad_pixels(image, x0, y0, x, y, color);
+        render_func(x0, y0, x, y);
     }
 
     // Left and right regions.
@@ -347,7 +390,7 @@ pub fn draw_hollow_ellipse_mut<I>(image: &mut I, center: (i32, i32), width_radiu
             p += (w2 - py + px) as f32;
         }
 
-        draw_quad_pixels(image, x0, y0, x, y, color);
+        render_func(x0, y0, x, y);
     }
 }
 
@@ -990,6 +1033,26 @@ mod test {
     bench_hollow_ellipse!(bench_bench_hollow_ellipse_circle, (200, 200), 80, 80);
     bench_hollow_ellipse!(bench_bench_hollow_ellipse_vertical, (200, 200), 40, 100);
     bench_hollow_ellipse!(bench_bench_hollow_ellipse_horizontal, (200, 200), 100, 40);
+
+    macro_rules! bench_filled_ellipse {
+        ($name:ident, $center:expr, $width_radius:expr, $height_radius:expr) => {
+            #[bench]
+            fn $name(b: &mut test::Bencher) {
+                use super::draw_filled_ellipse_mut;
+
+                let mut image = GrayImage::new(500, 500);
+                let color = Luma([50u8]);
+                b.iter(|| {
+                    draw_filled_ellipse_mut(&mut image, $center, $width_radius, $height_radius, color);
+                    test::black_box(&image);
+                    });
+            }
+        }
+    }
+
+    bench_filled_ellipse!(bench_bench_filled_ellipse_circle, (200, 200), 80, 80);
+    bench_filled_ellipse!(bench_bench_filled_ellipse_vertical, (200, 200), 40, 100);
+    bench_filled_ellipse!(bench_bench_filled_ellipse_horizontal, (200, 200), 100, 40);
 
     #[test]
     #[cfg_attr(rustfmt, rustfmt_skip)]
