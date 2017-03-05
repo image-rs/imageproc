736920b68eb1d6c19e7626f058e9310e9c23c285
mhsjlw <mhsjlw@users.noreply.github.com>
2017-03-05 08:29:02 -0500

    remove height from parameter documentation

--- src/drawing.rs
+++ src/drawing.rs
@@ -12,7 +12,7 @@ use std::i32;
 use pixelops::weighted_sum;
 use rusttype::{FontCollection, Scale, point, PositionedGlyph};
 
-/// Draws colored text on an image in place. `height` is your desired font height (in pixels), `scale` is augmented font scaling on both the x and y axis (in pixels). Note that this function *does not* support newlines, you must do this manually
+/// Draws colored text on an image in place. `scale` is augmented font scaling on both the x and y axis (in pixels). Note that this function *does not* support newlines, you must do this manually
 pub fn draw_text_mut<I>(image: &mut I, color: I::Pixel, x: u32, y: u32, scale: Scale, font_data: &[u8], text: &str)
     where I: GenericImage,
           <I::Pixel as Pixel>::Subpixel: ValueInto<f32> + Clamp<f32>
@@ -44,7 +44,7 @@ pub fn draw_text_mut<I>(image: &mut I, color: I::Pixel, x: u32, y: u32, scale: S
     }
 }
 
-/// Draws colored text on an image in place. `height` is your desired font height (in pixels), `scale` is augmented font scaling on both the x and y axis (in pixels). Note that this function *does not* support newlines, you must do this manually
+/// Draws colored text on an image in place. `scale` is augmented font scaling on both the x and y axis (in pixels). Note that this function *does not* support newlines, you must do this manually
 pub fn draw_text<I>(image: &mut I, color: I::Pixel, x: u32, y: u32, scale: Scale, font_data: &[u8], text: &str) -> Image<I::Pixel>
     where I: GenericImage,
           <I::Pixel as Pixel>::Subpixel: ValueInto<f32> + Clamp<f32>,
