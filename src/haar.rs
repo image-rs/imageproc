2dbac45266fb3ecb9421313b8e88d239e4af3545
Phil Ellison <phil.j.ellison@gmail.com>
2017-06-02 19:26:21 +0100

    Slightly reduce pointless newtype wrapping and unwrapping in haar.rs

--- src/haar.rs
+++ src/haar.rs
@@ -4,6 +4,7 @@ use definitions::{HasBlack, HasWhite, Image};
 use integralimage;
 use image::{GenericImage, ImageBuffer, Luma};
 use itertools::Itertools;
+use std::marker::PhantomData;
 use std::ops::Range;
 
 /// A Haar-like filter.
@@ -19,7 +20,7 @@ pub struct HaarFilter {
 /// Whether the top left region in a Haar filter is counted
 /// with positive or negative sign.
 #[derive(Copy, Clone, PartialEq, Eq, Debug)]
-pub enum Sign {
+enum Sign {
     /// Top left region is counted with a positive sign.
     Positive,
     /// Top left region is counted with a negative sign.
@@ -81,11 +82,11 @@ impl FilterType {
     // The width and height of a filter shape, in blocks.
     fn shape(&self) -> Size<Blocks> {
         match *self {
-            FilterType::TwoRegionHorizontal => Size::new(Blocks(2), Blocks(1)),
-            FilterType::ThreeRegionHorizontal => Size::new(Blocks(3), Blocks(1)),
-            FilterType::TwoRegionVertical => Size::new(Blocks(1), Blocks(2)),
-            FilterType::ThreeRegionVertical => Size::new(Blocks(1), Blocks(3)),
-            FilterType::FourRegion => Size::new(Blocks(2), Blocks(2))
+            FilterType::TwoRegionHorizontal => Size::new(2, 1),
+            FilterType::ThreeRegionHorizontal => Size::new(3, 1),
+            FilterType::TwoRegionVertical => Size::new(1, 2),
+            FilterType::ThreeRegionVertical => Size::new(1, 3),
+            FilterType::FourRegion => Size::new(2, 2)
         }
     }
 }
@@ -93,23 +94,18 @@ impl FilterType {
 impl HaarFilter {
     /// Evaluates the Haar filter on an integral image.
     pub fn evaluate(&self, integral: &Image<Luma<u32>>) -> i32 {
-        let blocks_wide = self.filter_type.shape().width.0;
-        let blocks_high = self.filter_type.shape().height.0;
-        let block_width = self.block_size.width.0;
-        let block_height = self.block_size.height.0;
-
         let parity_shift = if self.sign == Sign::Positive { 0 } else { 1 };
 
         let mut sum = 0i32;
 
         // TODO: this does too much work - we don't need to evaluate all of the regions individually
-        for w in 0..blocks_wide {
-            let left = self.left + block_width * w;
-            let right = left + block_width - 1;
+        for w in 0..self.blocks_wide() {
+            let left = self.left + self.block_width() * w;
+            let right = left + self.block_width() - 1;
 
-            for h in 0..blocks_high {
-                let top = self.top + block_height * h;
-                let bottom = top + block_height - 1;
+            for h in 0..self.blocks_high() {
+                let top = self.top + self.block_height() * h;
+                let bottom = top + self.block_height() - 1;
 
                 let parity = (w + h + parity_shift) % 2;
                 let multiplier = if parity == 0 { 1i32 } else { -1i32 };
@@ -121,20 +117,37 @@ impl HaarFilter {
 
         sum
     }
+
+    /// Width of this filter in blocks.
+    fn blocks_wide(&self) -> u8 {
+        self.filter_type.shape().width
+    }
+
+    /// Height of this filter in blocks.
+    fn blocks_high(&self) -> u8 {
+        self.filter_type.shape().height
+    }
+
+    /// Width of each block in pixels.
+    fn block_width(&self) -> u8 {
+        self.block_size.width
+    }
+
+    /// Height of each block in pixels.
+    fn block_height(&self) -> u8 {
+        self.block_size.height
+    }
 }
 
 // The total width and height of a filter with the given type and block size.
 fn filter_size(filter_type: FilterType, block_size: Size<Pixels>) -> Size<Pixels> {
     let shape = filter_type.shape();
-    Size {
-        width: Pixels(shape.width.0 * block_size.width.0),
-        height: Pixels(shape.height.0 * block_size.height.0)
-    }
+    Size::new(shape.width * block_size.width, shape.height * block_size.height)
 }
 
 /// Returns a vector of all valid Haar filters for an image with given width and height.
 pub fn enumerate_haar_filters(frame_width: u8, frame_height: u8) -> Vec<HaarFilter> {
-    let frame_size = Size::new(Pixels(frame_width), Pixels(frame_height));
+    let frame_size = Size::new(frame_width, frame_height);
 
     let filter_types = vec![
         FilterType::TwoRegionHorizontal,
@@ -164,43 +177,37 @@ fn haar_filters_of_type(filter_type: FilterType, frame_size: Size<Pixels>) -> Ve
     filters
 }
 
-// A marker trait to indicate that a type defines the units that some size is measured in.
-trait Unit {}
-
-// A size measured in pixels, e.g. the width of an individual block
-// within a Haar-like filter.
+// Indicates that a size size is measured in pixels, e.g. the width of an individual block within a Haar-like filter.
 #[derive(Copy, Clone, Hash, PartialEq, Eq, Debug, PartialOrd, Ord)]
 struct Pixels(u8);
 
-impl Unit for Pixels {}
-
-// A size measured in blocks, e.g. the width of a Haar-like filter in blocks.
+// Indicates that a size is measured in blocks, e.g. the width of a Haar-like filter in blocks.
 #[derive(Copy, Clone, Hash, PartialEq, Eq, Debug, PartialOrd, Ord)]
 struct Blocks(u8);
-
-impl Unit for Blocks {}
 
 // A Size, measured either in pixels (T = Pixels) or in blocks (T = Blocks)
 #[derive(Copy, Clone, Hash, PartialEq, Eq, Debug)]
 struct Size<T> {
-    width: T,
-    height: T,
+    width: u8,
+    height: u8,
+    units: PhantomData<T>
 }
 
-impl<T: Unit> Size<T> {
-    fn new(width: T, height: T) -> Size<T> {
+impl<T> Size<T> {
+    fn new(width: u8, height: u8) -> Size<T> {
         Size { 
-            width: width, 
-            height: height 
+            width: width,
+            height: height,
+            units: PhantomData
         }
     }
 }
 
 // Returns the valid block sizes for a feature of shape `feature_shape` in a frame of size `frame_size`.
 fn block_sizes(feature_shape: Size<Blocks>, frame_size: Size<Pixels>) -> Vec<Size<Pixels>> {
-    (1..frame_size.width.0 / feature_shape.width.0 + 1)
-        .cartesian_product(1..frame_size.height.0 / feature_shape.height.0 + 1)
-        .map(|(w, h)| Size::new(Pixels(w), Pixels(h)))
+    (1..frame_size.width / feature_shape.width + 1)
+        .cartesian_product(1..frame_size.height / feature_shape.height + 1)
+        .map(|(w, h)| Size::new(w, h))
         .collect()
 }
 
@@ -213,8 +220,8 @@ fn start_positions(inner: u8, outer: u8) -> Range<u8> {
 
 // Returns all valid (left, top) coordinates for a filter of the given total size
 fn filter_positions(feature_size: Size<Pixels>, frame_size: Size<Pixels>) -> Vec<(u8, u8)> {
-    start_positions(feature_size.width.0, frame_size.width.0)
-        .cartesian_product(start_positions(feature_size.height.0, frame_size.height.0))
+    start_positions(feature_size.width, frame_size.width)
+        .cartesian_product(start_positions(feature_size.height, frame_size.height))
         .collect()
 }
 
@@ -268,22 +275,16 @@ pub fn draw_haar_filter_mut<I>(image: &mut I, filter: HaarFilter)
     where I: GenericImage,
           I::Pixel: HasBlack + HasWhite
 {
-    let shape = filter.filter_type.shape();
-    let blocks_wide = shape.width.0;
-    let blocks_high = shape.height.0;
-    let block_width = filter.block_size.width.0;
-    let block_height = filter.block_size.height.0;
-
     let parity_shift = if filter.sign == Sign::Positive { 0 } else { 1 };
 
-    for w in 0..blocks_wide {
-        for h in 0..blocks_high {
+    for w in 0..filter.blocks_wide() {
+        for h in 0..filter.blocks_high() {
             let parity = (w + h + parity_shift) % 2;
             let color = if parity == 0 { I::Pixel::white() } else { I::Pixel::black() };
-            for x in 0..block_width {
-                for y in 0..block_height {
-                    let px = filter.left + w * block_width + x;
-                    let py = filter.top + h * block_height + y;
+            for x in 0..filter.block_width() {
+                for y in 0..filter.block_height() {
+                    let px = filter.left + w * filter.block_width() + x;
+                    let py = filter.top + h * filter.block_height() + y;
                     image.put_pixel(px as u32, py as u32, color);
                 }
             }
@@ -301,31 +302,35 @@ mod test {
 
     #[test]
     fn test_block_sizes() {
-        assert_eq!(block_sizes(FilterType::TwoRegionHorizontal.shape(), Size::new(Pixels(1), Pixels(1))),
+        assert_eq!(
+            block_sizes(FilterType::TwoRegionHorizontal.shape(), Size::new(1, 1)), 
             vec![]);
 
-        assert_eq!(block_sizes(FilterType::TwoRegionHorizontal.shape(), Size::new(Pixels(2), Pixels(1))),
-            vec![Size::new(Pixels(1), Pixels(1))]);
+        assert_eq!(
+            block_sizes(FilterType::TwoRegionHorizontal.shape(), Size::new(2, 1)), 
+            vec![Size::new(1, 1)]);
 
-        assert_eq!(block_sizes(FilterType::TwoRegionHorizontal.shape(), Size::new(Pixels(5), Pixels(1))),
-            vec![Size::new(Pixels(1), Pixels(1)), Size::new(Pixels(2), Pixels(1))]);
+        assert_eq!(
+            block_sizes(FilterType::TwoRegionHorizontal.shape(), Size::new(5, 1)),
+            vec![Size::new(1, 1), Size::new(2, 1)]);
 
-        assert_eq!(block_sizes(FilterType::TwoRegionVertical.shape(), Size::new(Pixels(1), Pixels(2))),
-            vec![Size::new(Pixels(1), Pixels(1))]);
+        assert_eq!(
+            block_sizes(FilterType::TwoRegionVertical.shape(), Size::new(1, 2)),
+            vec![Size::new(1, 1)]);
     }
 
     #[test]
     fn test_filter_positions() {
-        assert_eq!(filter_positions(Size::new(Pixels(2), Pixels(3)), Size::new(Pixels(2), Pixels(2))), 
+        assert_eq!(filter_positions(Size::new(2, 3), Size::new(2, 2)), 
             vec![]);
 
-        assert_eq!(filter_positions(Size::new(Pixels(2), Pixels(2)), Size::new(Pixels(2), Pixels(2))), 
+        assert_eq!(filter_positions(Size::new(2, 2), Size::new(2, 2)), 
             vec![(0, 0)]);
 
-        assert_eq!(filter_positions(Size::new(Pixels(2), Pixels(2)), Size::new(Pixels(3), Pixels(2))), 
+        assert_eq!(filter_positions(Size::new(2, 2), Size::new(3, 2)), 
             vec![(0, 0), (1, 0)]);
 
-        assert_eq!(filter_positions(Size::new(Pixels(2), Pixels(2)), Size::new(Pixels(3), Pixels(3))), 
+        assert_eq!(filter_positions(Size::new(2, 2), Size::new(3, 3)), 
             vec![(0, 0), (0, 1), (1, 0), (1, 1)]);
     }
 
@@ -356,7 +361,7 @@ mod test {
         let filter = HaarFilter { 
             sign: Sign::Positive,
             filter_type: FilterType::TwoRegionHorizontal,
-            block_size: Size::new(Pixels(2), Pixels(3)),
+            block_size: Size::new(2, 3),
             left: 1,
             top: 1
         };
@@ -380,7 +385,7 @@ mod test {
         let filter = HaarFilter { 
             sign: Sign::Negative,
             filter_type: FilterType::ThreeRegionVertical,
-            block_size: Size::new(Pixels(2), Pixels(1)),
+            block_size: Size::new(2, 1),
             left: 0,
             top: 0
         };
@@ -403,7 +408,7 @@ mod test {
         let filter = HaarFilter { 
             sign: Sign::Positive,
             filter_type: FilterType::FourRegion,
-            block_size: Size::new(Pixels(2), Pixels(2)),
+            block_size: Size::new(2, 2),
             left: 1,
             top: 0
         };
@@ -425,7 +430,7 @@ mod test {
         let filter = HaarFilter { 
             sign: Sign::Positive,
             filter_type: FilterType::TwoRegionHorizontal,
-            block_size: Size::new(Pixels(2), Pixels(3)),
+            block_size: Size::new(2, 3),
             left: 1,
             top: 1
         };
@@ -458,7 +463,7 @@ mod test {
         let filter = HaarFilter { 
             sign: Sign::Positive,
             filter_type: FilterType::FourRegion,
-            block_size: Size::new(Pixels(2), Pixels(2)),
+            block_size: Size::new(2, 2),
             left: 1,
             top: 0
         };
