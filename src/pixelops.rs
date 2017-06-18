395a162739a2117c4bbf25f27037460fc650730e
Phil Ellison <phil.j.ellison@gmail.com>
2017-06-18 15:19:23 +0100

    Run rustfmt on pixelops.rs

--- src/pixelops.rs
+++ src/pixelops.rs
@@ -7,21 +7,26 @@ use definitions::Clamp;
 
 /// Adds pixels with the given weights. Results are clamped to prevent arithmetical overflows.
 pub fn weighted_sum<P: Pixel>(left: P, right: P, left_weight: f32, right_weight: f32) -> P
-    where P::Subpixel: ValueInto<f32> + Clamp<f32>
+where
+    P::Subpixel: ValueInto<f32> + Clamp<f32>,
 {
-    left.map2(&right, |p, q| weighted_channel_sum(p, q, left_weight, right_weight))
+    left.map2(&right, |p, q| {
+        weighted_channel_sum(p, q, left_weight, right_weight)
+    })
 }
 
 /// Equivalent to `weighted_sum(left, right, left_weight, 1 - left_weight).
 pub fn interpolate<P: Pixel>(left: P, right: P, left_weight: f32) -> P
-    where P::Subpixel: ValueInto<f32> + Clamp<f32>
+where
+    P::Subpixel: ValueInto<f32> + Clamp<f32>,
 {
     weighted_sum(left, right, left_weight, 1.0 - left_weight)
 }
 
 #[inline(always)]
 fn weighted_channel_sum<C>(left: C, right: C, left_weight: f32, right_weight: f32) -> C
-    where C: ValueInto<f32> + Clamp<f32>
+where
+    C: ValueInto<f32> + Clamp<f32>,
 {
     Clamp::clamp(cast(left) * left_weight + cast(right) * right_weight)
 }
