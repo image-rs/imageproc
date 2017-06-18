e79ff68eb770f1f52a3774d07a45cadff22fe8f8
Phil Ellison <phil.j.ellison@gmail.com>
2017-06-18 15:00:51 +0100

    Run rustfmt on edges.rs

--- src/edges.rs
+++ src/edges.rs
@@ -7,7 +7,7 @@ use definitions::{HasWhite, HasBlack};
 use filter::gaussian_blur_f32;
 
 /// Runs the canny edge detection algorithm.
-/// 
+///
 /// Returns a binary image where edge pixels have a value of 255
 ///  and non-edge pixels a value of 0.
 ///
@@ -27,10 +27,7 @@ use filter::gaussian_blur_f32;
 /// definition of edge strength: the strength of an edge at a point `p` is
 /// defined to be `sqrt(dx^2 + dy^2)`, where `dx` and `dy` are the values
 /// of the horizontal and vertical Sobel gradients at `p`.
-pub fn canny(image: &GrayImage,
-             low_threshold: f32,
-             high_threshold: f32)
-             -> GrayImage {
+pub fn canny(image: &GrayImage, low_threshold: f32, high_threshold: f32) -> GrayImage {
     assert!(high_threshold >= low_threshold);
     // Heavily based on the implementation proposed by wikipedia.
     // 1. Gaussian blur.
@@ -41,9 +38,9 @@ pub fn canny(image: &GrayImage,
     let gx = horizontal_sobel(&blurred);
     let gy = vertical_sobel(&blurred);
     let g: Vec<f32> = gx.iter()
-                        .zip(gy.iter())
-                        .map(|(h, v)| (*h as f32).hypot(*v as f32))
-                        .collect::<Vec<f32>>();
+        .zip(gy.iter())
+        .map(|(h, v)| (*h as f32).hypot(*v as f32))
+        .collect::<Vec<f32>>();
 
     let g = ImageBuffer::from_raw(image.width(), image.height(), g).unwrap();
 
@@ -55,10 +52,11 @@ pub fn canny(image: &GrayImage,
 }
 
 /// Finds local maxima to make the edges thinner.
-fn non_maximum_suppression(g: &ImageBuffer<Luma<f32>, Vec<f32>>,
-                           gx: &ImageBuffer<Luma<i16>, Vec<i16>>,
-                           gy: &ImageBuffer<Luma<i16>, Vec<i16>>)
-                           -> ImageBuffer<Luma<f32>, Vec<f32>> {
+fn non_maximum_suppression(
+    g: &ImageBuffer<Luma<f32>, Vec<f32>>,
+    gx: &ImageBuffer<Luma<i16>, Vec<i16>>,
+    gy: &ImageBuffer<Luma<i16>, Vec<i16>>,
+) -> ImageBuffer<Luma<f32>, Vec<f32>> {
     const RADIANS_TO_DEGREES: f32 = 180f32 / f32::consts::PI;
     let mut out = ImageBuffer::from_pixel(g.width(), g.height(), Luma { data: [0.0] });
     for y in 1..g.height() - 1 {
@@ -87,13 +85,17 @@ fn non_maximum_suppression(g: &ImageBuffer<Luma<f32>, Vec<f32>>,
                 match clamped_angle {
                     0 => (g.unsafe_get_pixel(x - 1, y), g.unsafe_get_pixel(x + 1, y)),
                     45 => {
-                        (g.unsafe_get_pixel(x + 1, y + 1),
-                         g.unsafe_get_pixel(x - 1, y - 1))
+                        (
+                            g.unsafe_get_pixel(x + 1, y + 1),
+                            g.unsafe_get_pixel(x - 1, y - 1),
+                        )
                     }
                     90 => (g.unsafe_get_pixel(x, y - 1), g.unsafe_get_pixel(x, y + 1)),
                     135 => {
-                        (g.unsafe_get_pixel(x - 1, y + 1),
-                         g.unsafe_get_pixel(x + 1, y - 1))
+                        (
+                            g.unsafe_get_pixel(x - 1, y + 1),
+                            g.unsafe_get_pixel(x + 1, y - 1),
+                        )
                     }
                     _ => unreachable!(),
                 }
@@ -112,10 +114,11 @@ fn non_maximum_suppression(g: &ImageBuffer<Luma<f32>, Vec<f32>>,
 
 /// Filter out edges with the thresholds.
 /// Non-recursive breadth-first search.
-fn hysteresis(input: &ImageBuffer<Luma<f32>, Vec<f32>>,
-              low_thresh: f32,
-              high_thresh: f32)
-              -> ImageBuffer<Luma<u8>, Vec<u8>> {
+fn hysteresis(
+    input: &ImageBuffer<Luma<f32>, Vec<f32>>,
+    low_thresh: f32,
+    high_thresh: f32,
+) -> ImageBuffer<Luma<u8>, Vec<u8>> {
 
     let max_brightness = Luma::white();
     let min_brightness = Luma::black();
@@ -134,12 +137,14 @@ fn hysteresis(input: &ImageBuffer<Luma<f32>, Vec<f32>>,
                 // Track neighbors until no neighbor is >= low_thresh.
                 while !edges.is_empty() {
                     let (nx, ny) = edges.pop().unwrap();
-                    let neighbor_indices = [(nx + 1, ny),
-                                            (nx + 1, ny + 1),
-                                            (nx, ny + 1),
-                                            (nx - 1, ny - 1),
-                                            (nx - 1, ny),
-                                            (nx - 1, ny + 1)];
+                    let neighbor_indices = [
+                        (nx + 1, ny),
+                        (nx + 1, ny + 1),
+                        (nx, ny + 1),
+                        (nx - 1, ny - 1),
+                        (nx - 1, ny),
+                        (nx - 1, ny + 1),
+                    ];
 
                     for neighbor_idx in &neighbor_indices {
                         let in_neighbor = *input.get_pixel(neighbor_idx.0, neighbor_idx.1);
@@ -159,7 +164,7 @@ fn hysteresis(input: &ImageBuffer<Luma<f32>, Vec<f32>>,
 #[cfg(test)]
 mod test {
     use super::canny;
-    use drawing::{draw_filled_rect_mut};
+    use drawing::draw_filled_rect_mut;
     use rect::Rect;
     use image::{GrayImage, Luma};
     use test;
