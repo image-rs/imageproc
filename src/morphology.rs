8cdb945f2291b0070a98acfb8c0571f9be8207bb
Phil Ellison <phil.j.ellison@gmail.com>
2017-10-30 21:11:26 +0000

    Fix test

--- src/morphology.rs
+++ src/morphology.rs
@@ -484,14 +484,14 @@ mod test {
 
     #[test]
     fn test_distance_transform_saturation() {
-        // A single foreground pixel in the top-right
-        let image = GrayImage::from_fn(5, 5, |x, y| match (x, y) {
+        // A single foreground pixel in the top-left
+        let image = GrayImage::from_fn(300, 300, |x, y| match (x, y) {
             (0, 0) => Luma([255u8]),
             _ => Luma([0u8]),
         });
 
         // Distances should not overflow
-        let expected = GrayImage::from_fn(5, 5, |x, y| Luma([min(255, max(x, y)) as u8]));
+        let expected = GrayImage::from_fn(300, 300, |x, y| Luma([min(255, max(x, y)) as u8]));
 
         let distances = distance_transform(&image, Norm::LInf);
         assert_pixels_eq!(distances, expected);
