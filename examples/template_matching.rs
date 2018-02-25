75ff050abcef815497b87d4efdb4681b03fcace5
Phil Ellison <phil.j.ellison@gmail.com>
2018-02-25 19:12:37 +0000

    Use both match methods in match_template example

--- examples/template_matching.rs
+++ examples/template_matching.rs
@@ -13,6 +13,41 @@ use imageproc::map::map_colors;
 use imageproc::rect::Rect;
 use imageproc::drawing::draw_hollow_rect_mut;
 
+struct TemplateMatchingArgs {
+    input_path: PathBuf,
+    output_dir: PathBuf,
+    template_x: u32,
+    template_y: u32,
+    template_w: u32,
+    template_h: u32
+}
+
+impl TemplateMatchingArgs {
+    fn parse(args: Vec<String>) -> TemplateMatchingArgs {
+        if args.len() != 7 {
+            panic!(r#"
+Usage:
+
+     cargo run --example template_matching input_path output_dir template_x template_y template_w template_h
+
+Loads the image at input_path and extracts a region with the given location and size to use as the matching
+template. Calls match_template on the input image and this template, and saves the results to output_dir.
+"#);
+        }
+
+        let input_path = PathBuf::from(&args[1]);
+        let output_dir = PathBuf::from(&args[2]);
+        let template_x = args[3].parse().unwrap();
+        let template_y = args[4].parse().unwrap();
+        let template_w = args[5].parse().unwrap();
+        let template_h = args[6].parse().unwrap();
+
+        TemplateMatchingArgs {
+            input_path, output_dir, template_x, template_y, template_w, template_h
+        }
+    }
+}
+
 /// Convert an f32-valued image to a 8 bit depth, covering the whole
 /// available intensity range.
 fn convert_to_gray_image(image: &Image<Luma<f32>>) -> GrayImage {
@@ -50,39 +85,24 @@ fn draw_green_rect(image: &GrayImage, rect: Rect) -> RgbImage {
     color_image
 }
 
-struct TemplateMatchingArgs {
-    input_path: PathBuf,
-    output_dir: PathBuf,
-    template_x: u32,
-    template_y: u32,
-    template_w: u32,
-    template_h: u32
-}
+fn run_match_template(
+    args: &TemplateMatchingArgs,
+    image: &GrayImage,
+    template: &GrayImage,
+    method: MatchTemplateMethod) -> RgbImage {
+    // Match the template and convert to u8 depth to display
+    let result = match_template(&image, &template, method);
+    let result_scaled = convert_to_gray_image(&result);
 
-impl TemplateMatchingArgs {
-    fn parse(args: Vec<String>) -> TemplateMatchingArgs {
-        if args.len() != 7 {
-            panic!(r#"
-Usage:
+    // Pad the result to the same size as the input image, to make them easier to compare
+    let mut result_padded = GrayImage::new(image.width(), image.height());
+    result_padded.copy_from(&result_scaled, args.template_w / 2, args.template_h / 2);
 
-     cargo run --example template_matching input_path output_dir template_x template_y template_w template_h
+    // Show location the template was extracted from
+    let roi = Rect::at(args.template_x as i32, args.template_y as i32)
+        .of_size(args.template_w, args.template_h);
 
-Loads the image at input_path and extracts a region with the given location and size to use as the matching
-template. Calls match_template on the input image and this template, and saves the results to output_dir.
-"#);
-        }
-
-        let input_path = PathBuf::from(&args[1]);
-        let output_dir = PathBuf::from(&args[2]);
-        let template_x = args[3].parse().unwrap();
-        let template_y = args[4].parse().unwrap();
-        let template_w = args[5].parse().unwrap();
-        let template_h = args[6].parse().unwrap();
-
-        TemplateMatchingArgs {
-            input_path, output_dir, template_x, template_y, template_w, template_h
-        }
-    }
+    draw_green_rect(&result_padded, roi)
 }
 
 fn main() {
@@ -107,26 +127,23 @@ fn main() {
     // Extract the requested image sub-region to use as the template
     let template = copy_sub_image(&image, args.template_x, args.template_y, args.template_w, args.template_h);
 
-    // Match the template and convert to u8 depth to display
-    let result = match_template(&image, &template, MatchTemplateMethod::SumOfSquaredErrorsNormalized);
-    let result_scaled = convert_to_gray_image(&result);
-
-    // Pad the result to the same size as the input image, to make them easier to compare
-    let mut result_padded = GrayImage::new(image.width(), image.height());
-    result_padded.copy_from(&result_scaled, args.template_w / 2, args.template_h / 2);
+    // Match using all available match methods
+    let sse = run_match_template(&args, &image, &template, MatchTemplateMethod::SumOfSquaredErrors);
+    let sse_norm = run_match_template(&args, &image, &template, MatchTemplateMethod::SumOfSquaredErrorsNormalized);
 
     // Show location the template was extracted from
     let roi = Rect::at(args.template_x as i32, args.template_y as i32)
         .of_size(args.template_w, args.template_h);
 
     let image_with_roi = draw_green_rect(&image, roi);
-    let result_with_roi = draw_green_rect(&result_padded, roi);
-
+  
     // Save images to output_dir
     let template_path = output_dir.join("template.png");
     template.save(&template_path).unwrap();
     let source_path = output_dir.join("image.png");
     image_with_roi.save(&source_path).unwrap();
-    let result_path = output_dir.join("result.png");
-    result_with_roi.save(&result_path).unwrap();
+    let sse_path = output_dir.join("result_sse.png");
+    sse.save(&sse_path).unwrap();
+    let sse_path = output_dir.join("result_sse_norm.png");
+    sse_norm.save(&sse_path).unwrap();
 }
