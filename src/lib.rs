092fd8b0f391b589025173706590d5063124fea4
Phil Ellison <phil.j.ellison@gmail.com>
2018-12-14 07:07:06 +0000

    Mention imageops module in image crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,12 @@
-//! An image processing library, based on the
-//! [image](https://github.com/PistonDevelopers/image) crate.
+//! An image processing library based on the
+//! [image] crate.
+//!
+//! Note that the image crate contains some image
+//! processing functions (including image resizing) in its
+//! `imageops` module, so check there if you cannot find
+//! a standard image processing function in this crate.
+//!
+//! [image]: https://github.com/PistonDevelopers/image
 #![deny(missing_docs)]
 #![cfg_attr(test, feature(test))]
 
