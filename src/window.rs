ebe4c868a223272b769c5bee0ddab1f366d3846b
lazypassion <25536767+lazypassion@users.noreply.github.com>
2019-01-21 19:49:19 -0500

    added suggested fixes, renamed multiple_image_displays.rs to display_multiple_images.rs

--- src/window.rs
+++ src/window.rs
@@ -6,7 +6,9 @@ use sdl2::{
     keyboard::Keycode,
     pixels::{Color, PixelFormatEnum},
     rect::Rect,
+    render::{Canvas, TextureCreator, WindowCanvas},
     surface::Surface,
+    video::{Window, WindowContext},
 };
 
 /// Displays the provided RGBA image in a new window.
@@ -23,18 +25,11 @@ pub fn display_image(title: &str, image: &RgbaImage, window_width: u32, window_h
 /// will be rounded up to the minimum.
 pub fn display_multiple_images(
     title: &str,
-    images: &Vec<&RgbaImage>,
+    images: &[&RgbaImage],
     window_width: u32,
     window_height: u32,
 ) {
-    use sdl2::render::Canvas;
-    use sdl2::render::TextureCreator;
-    use sdl2::render::WindowCanvas;
-    use sdl2::video::Window;
-    use sdl2::video::WindowContext;
-
     if images.len() == 0 {
-        println!("Must provide at least one image for display.");
         return;
     }
 
@@ -47,21 +42,47 @@ pub fn display_multiple_images(
     let sdl = sdl2::init().expect("couldn't create sdl2 context");
     let video_subsystem = sdl.video().expect("couldn't create video subsystem");
 
-    let mut windows: Vec<sdl2::video::Window> = Vec::with_capacity(images.len());
+    let mut windows: Vec<Window> = Vec::with_capacity(images.len());
     let mut window_visibility: Vec<bool> = Vec::with_capacity(images.len());
     for _ in 0..images.len() {
         let mut window = video_subsystem
             .window(title, window_width, window_height)
-            .position_centered()
             .resizable()
+            .allow_highdpi()
             .build()
             .expect("couldn't create window");
 
         window
             .set_minimum_size(MIN_WINDOW_DIMENSION, MIN_WINDOW_DIMENSION)
             .expect("invalid minimum size for window");
+
         windows.push(window);
         window_visibility.push(true);
+    }
+
+    {
+        use sdl2::video::WindowPos::Positioned;
+
+        let (base_position_x, base_position_y) = windows[0].position();
+        for (i, window) in windows.iter_mut().enumerate() {
+            let multiplier = 1.0 + i as f32 / 20.0;
+            window.set_position(
+                Positioned((base_position_x as f32 * multiplier) as i32),
+                Positioned(base_position_y),
+            );
+
+            let (window_pos_x, _window_pos_y) = window.position();
+            let display_bounds = video_subsystem
+                .display_bounds(0)
+                .expect("No bounds found for that display.");
+            let screen_width = display_bounds.w;
+            if window_pos_x + window_width as i32 > screen_width {
+                window.set_position(
+                    Positioned(screen_width - window_width as i32),
+                    Positioned(base_position_y),
+                );
+            }
+        }
     }
 
     let mut canvases: Vec<WindowCanvas> = Vec::with_capacity(images.len());
@@ -74,8 +95,7 @@ pub fn display_multiple_images(
         canvases.push(canvas);
     }
 
-    let mut texture_creators: Vec<sdl2::render::TextureCreator<WindowContext>> =
-        Vec::with_capacity(images.len());
+    let mut texture_creators: Vec<TextureCreator<WindowContext>> = Vec::with_capacity(images.len());
     for canvas in canvases.iter() {
         let texture_creator = canvas.texture_creator();
         texture_creators.push(texture_creator);
