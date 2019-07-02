89327be526a6d3a66644be184047711f7e32f7db
Phil Ellison <phil.j.ellison@gmail.com>
2019-07-02 23:07:07 +0100

    Make conic drawing functions generic over Canvas

--- src/drawing/mod.rs
+++ src/drawing/mod.rs
@@ -62,16 +62,14 @@ pub use self::text::{
     draw_text_mut
 };
 
-use image::GenericImage;
-
 // Set pixel at (x, y) to color if this point lies within image bounds,
 // otherwise do nothing.
-fn draw_if_in_bounds<I>(image: &mut I, x: i32, y: i32, color: I::Pixel)
+fn draw_if_in_bounds<C>(canvas: &mut C, x: i32, y: i32, color: C::Pixel)
 where
-    I: GenericImage,
-    I::Pixel: 'static,
+    C: Canvas,
+    C::Pixel: image::Pixel + 'static,
 {
-    if x >= 0 && x < image.width() as i32 && y >= 0 && y < image.height() as i32 {
-        image.put_pixel(x as u32, y as u32, color);
+    if x >= 0 && x < canvas.width() as i32 && y >= 0 && y < canvas.height() as i32 {
+        canvas.draw_pixel(x as u32, y as u32, color);
     }
 }
