b24e596e0a6cd6db2e21113e094a506bde86970e
Phil Ellison <phil.j.ellison@gmail.com>
2019-07-06 19:40:08 +0100

    Improve documentation

--- src/drawing/canvas.rs
+++ src/drawing/canvas.rs
@@ -11,6 +11,46 @@ use image::{
 /// All instances of `GenericImage` implement `Canvas`, with
 /// the behaviour of `draw_pixel` being equivalent to calling
 /// `set_pixel` with the same arguments.
+///
+/// See [`Blend`](struct.Blend.html) for another example implementation
+/// of this trait - its implementation of `draw_pixel` alpha-blends
+/// the input value with the pixel's current value.
+///
+/// # Examples
+/// ```
+/// # extern crate image;
+/// # #[macro_use]
+/// # extern crate imageproc;
+/// # fn main() {
+/// use image::{Pixel, Rgba, RgbaImage};
+/// use imageproc::drawing::{Canvas, Blend};
+///
+/// // A trivial function which draws on a Canvas
+/// fn write_a_pixel<C: Canvas>(canvas: &mut C, c: C::Pixel) {
+///     canvas.draw_pixel(0, 0, c);
+/// }
+///
+/// // Background color
+/// let solid_blue = Rgba([0u8, 0u8, 255u8, 255u8]);
+///
+/// // Drawing color
+/// let translucent_red = Rgba([255u8, 0u8, 0u8, 127u8]);
+///
+/// // Blended combination of background and drawing colors
+/// let mut alpha_blended = solid_blue;
+/// alpha_blended.blend(&translucent_red);
+///
+/// // The implementation of Canvas for GenericImage overwrites existing pixels
+/// let mut image = RgbaImage::from_pixel(1, 1, solid_blue);
+/// write_a_pixel(&mut image, translucent_red);
+/// assert_eq!(*image.get_pixel(0, 0), translucent_red);
+///
+/// // This behaviour can be customised by using a different Canvas type
+/// let mut image = Blend(RgbaImage::from_pixel(1, 1, solid_blue));
+/// write_a_pixel(&mut image, translucent_red);
+/// assert_eq!(*image.0.get_pixel(0, 0), alpha_blended);
+/// # }
+/// ```
 pub trait Canvas {
     /// The type of `Pixel` that can be drawn on this canvas.
     type Pixel: Pixel;
@@ -50,6 +90,9 @@ where
 }
 
 /// A canvas that blends pixels when drawing.
+///
+/// See the documentation for [`Canvas`](trait.Canvas.html)
+/// for an example using this type.
 pub struct Blend<I>(pub I);
 
 impl<I: GenericImage> Canvas for Blend<I> {
