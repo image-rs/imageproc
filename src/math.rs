2553f08438381eb2332767439892063a8b90f5da
dimitri <dimitri.gallos@wrnch.ai>
2019-11-22 15:18:33 -0500

    added l1 norm in math

--- src/math.rs
+++ src/math.rs
@@ -2,6 +2,11 @@
 
 use conv::ValueInto;
 
+/// L1 norm of a vector.
+pub fn l1_norm(xs: &[f32]) -> f32 {
+    xs.iter().fold(0f32, |acc, x| acc + x.abs())
+}
+
 /// L2 norm of a vector.
 pub fn l2_norm(xs: &[f32]) -> f32 {
     xs.iter().fold(0f32, |acc, x| acc + x * x).sqrt()
