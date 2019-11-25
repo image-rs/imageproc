ddadddd24d2081b1725cd14984d230358d86e424
flappyBug <tdkgn@outlook.com>
2019-11-25 14:43:15 +0800

    Implement WindowIter

--- src/window.rs
+++ src/window.rs
@@ -2,7 +2,9 @@
 #![allow(missing_docs)]
 use image::GenericImageView;
 use image::{ImageBuffer, Pixel};
-use std::ops::Deref;
+use itertools::{Itertools, Product};
+use std::ops::{Deref, Range};
+
 pub struct Window<I, P> {
     pub image: I,
     pub xoffset: i32,
@@ -67,6 +69,7 @@ fn clamp<T: std::cmp::Ord>(v: T, lo: T, hi: T) -> T {
     }
 }
 
+#[derive(Debug, Copy, Clone)]
 pub enum PadOption<P> {
     Symmetric,
     Replicate,
@@ -74,10 +77,89 @@ pub enum PadOption<P> {
     Constant(P),
 }
 
+#[derive(Debug, Copy, Clone)]
 pub enum SizeOption {
     Same,
     Full,
     Valid,
 }
 
-pub trait ImageWindow {}
+pub struct WindowIter<I, P> {
+    image: I,
+    width: u32,
+    height: u32,
+    pad_option: PadOption<P>,
+    offset_iter: Product<Range<i32>, Range<i32>>,
+}
+
+impl<I: Copy, P: Pixel> Iterator for WindowIter<I, P> {
+    type Item = Window<I, P>;
+    fn next(&mut self) -> Option<Window<I, P>> {
+        if let Some((xoffset, yoffset)) = self.offset_iter.next() {
+            Some(Window {
+                image: self.image,
+                xoffset,
+                yoffset,
+                xstride: self.width,
+                ystride: self.height,
+                pad_option: self.pad_option,
+            })
+        } else {
+            None
+        }
+    }
+}
+
+pub trait ImageWindow<P: Pixel> {
+    fn windows(
+        &self,
+        width: u32,
+        height: u32,
+        size_option: SizeOption,
+        pad_option: PadOption<P>,
+    ) -> WindowIter<&Self, P>;
+}
+
+impl<P, C> ImageWindow<P> for ImageBuffer<P, C>
+where
+    P: Pixel + 'static,
+    C: Deref<Target = [P::Subpixel]> + Deref,
+    P::Subpixel: 'static,
+{
+    fn windows(
+        &self,
+        width: u32,
+        height: u32,
+        size_option: SizeOption,
+        pad_option: PadOption<P>,
+    ) -> WindowIter<&Self, P> {
+        use SizeOption::*;
+        let (image_width, image_height) = self.dimensions();
+        let (image_width, image_height) = (image_width as i32, image_height as i32);
+        let (width, height) = (width as i32, height as i32);
+        let offset_iter = match size_option {
+            Valid => {
+                let xrange = 0..(image_width - width + 1);
+                let yrange = 0..(image_height - height + 1);
+                Itertools::cartesian_product(yrange, xrange)
+            }
+            Full => {
+                let xrange = (-width + 1)..image_width;
+                let yrange = (-height + 1)..image_height;
+                Itertools::cartesian_product(yrange, xrange)
+            }
+            Same => {
+                let xrange = (-(width - 1) / 2)..(image_width - (width - 1) / 2);
+                let yrange = (-(height - 1) / 2)..(image_height - (height - 1) / 2);
+                Itertools::cartesian_product(yrange, xrange)
+            }
+        };
+        WindowIter {
+            image: self,
+            width: width as u32,
+            height: height as u32,
+            pad_option,
+            offset_iter,
+        }
+    }
+}
