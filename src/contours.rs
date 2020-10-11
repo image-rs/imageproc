757abcc738de79f3f87bca3366775fa9f7df11fc
Zoran Lazarevic <40909265+lazareviczoran@users.noreply.github.com>
2020-10-11 14:24:55 +0200

    Added functions for polygon approximation (#432)

--- src/contours.rs
+++ src/contours.rs
@@ -4,7 +4,9 @@
 use crate::definitions::Point;
 use image::GrayImage;
 use num::{cast, Num, NumCast};
+use std::cmp::Ordering;
 use std::collections::VecDeque;
+use std::f64::consts::PI;
 
 /// Contour struct containing its points, is_outer flag to determine whether the
 /// contour is an outer border or hole border, and the parent option (the index
@@ -196,10 +198,315 @@ fn get_position_if_non_zero_pixel(
     None
 }
 
+/// Returns the length of the arc constructed with the provided points in
+/// incremental order. When the `closed` param is set to `true`, the distance
+/// between the last and the first point is included in the total length.
+///
+pub fn arc_length<T: Num + NumCast + Copy + PartialEq + Eq>(arc: &[Point<T>], closed: bool) -> f64 {
+    if arc.len() < 2 {
+        return 0.;
+    }
+    let mut length = arc
+        .windows(2)
+        .fold(0., |acc, pts| acc + get_distance(&pts[0], &pts[1]));
+    if arc.len() > 2 && closed {
+        length += get_distance(&arc[0], &arc[arc.len() - 1]);
+    }
+    length
+}
+
+/// Fits the polygon curve to a similar curve with fewer points.
+/// The input parameters include an ordered array of points and an distance
+/// dimension `epsilon` > 0. Based on the [Douglas–Peucker algorithm].
+///
+/// [Douglas–Peucker algorithm]: https://en.wikipedia.org/wiki/Ramer-Douglas-Peucker_algorithm
+pub fn approx_poly_dp<T: Num + NumCast + Copy + PartialEq + Eq>(
+    curve: &[Point<T>],
+    epsilon: f64,
+    closed: bool,
+) -> Vec<Point<T>> {
+    if epsilon <= 0. {
+        panic!("epsilon must be greater than 0");
+    }
+    // Find the point with the maximum distance
+    let mut dmax = 0.;
+    let mut index = 0;
+    let end = curve.len() - 1;
+    let line_args = line_params(&curve[0], &curve[end]);
+    for (i, point) in curve.iter().enumerate().skip(1) {
+        let d = perpendicular_distance(line_args, point);
+        if d > dmax {
+            index = i;
+            dmax = d;
+        }
+    }
+
+    let mut res = Vec::new();
+
+    // If max distance is greater than epsilon, recursively simplify
+    if dmax > epsilon {
+        // Recursive call
+        let mut partial1 = approx_poly_dp(&curve[0..=index], epsilon, false);
+        let mut partial2 = approx_poly_dp(&curve[index..=end], epsilon, false);
+
+        // Build the result list
+        partial1.pop();
+        res.append(&mut partial1);
+        res.append(&mut partial2);
+    } else {
+        res.push(curve[0]);
+        res.push(curve[end]);
+    }
+
+    if closed {
+        res.pop();
+    }
+
+    res
+}
+
+/// Returns the parameters of the [line equation] (Ax + By + C = 0) that passes through the
+/// given points p1 and p2.
+///
+/// [line equation]: https://en.wikipedia.org/wiki/Linear_equation#Two-point_form
+fn line_params<T: Num + NumCast + Copy + PartialEq + Eq>(
+    p1: &Point<T>,
+    p2: &Point<T>,
+) -> (f64, f64, f64) {
+    let a = p1.y.to_f64().unwrap() - p2.y.to_f64().unwrap();
+    let b = p2.x.to_f64().unwrap() - p1.x.to_f64().unwrap();
+    let c = (p1.x * p2.y).to_f64().unwrap() - (p2.x * p1.y).to_f64().unwrap();
+
+    (a, b, c)
+}
+
+#[allow(clippy::many_single_char_names)]
+fn perpendicular_distance<T: Num + NumCast + Copy + PartialEq + Eq>(
+    line_args: (f64, f64, f64),
+    point: &Point<T>,
+) -> f64 {
+    let (a, b, c) = line_args;
+
+    (a * point.x.to_f64().unwrap() + b * point.y.to_f64().unwrap() + c).abs()
+        / (a.powf(2.) + b.powf(2.)).sqrt()
+}
+
+/// Finds the minimal area rectangle that covers all of the points in the input
+/// contour in the following order -> (TL, TR, BR, BL).
+///
+pub fn min_area_rect<T: Num + NumCast + Copy + PartialEq + Eq>(
+    contour: &[Point<T>],
+) -> Vec<Point<T>> {
+    let hull = convex_hull(&contour);
+    match hull.len() {
+        0 => panic!("no points are defined"),
+        1 => vec![hull[0]; 4],
+        2 => vec![hull[0], hull[1], hull[1], hull[0]],
+        _ => rotating_calipers(&hull),
+    }
+}
+
+/// The implementation of the [rotating calipers] used for determining the
+/// bounding rectangle with the smallest area.
+///
+/// [rotating calipers]: https://en.wikipedia.org/wiki/Rotating_calipers
+fn rotating_calipers<T: Num + NumCast + Copy + PartialEq + Eq>(
+    points: &[Point<T>],
+) -> Vec<Point<T>> {
+    let n = points.len();
+    let edges: Vec<(f64, f64)> = (0..n - 1)
+        .map(|i| {
+            let next = i + 1;
+            (
+                points[next].x.to_f64().unwrap() - points[i].x.to_f64().unwrap(),
+                points[next].y.to_f64().unwrap() - points[i].y.to_f64().unwrap(),
+            )
+        })
+        .collect();
+
+    let mut edge_angles: Vec<f64> = edges
+        .iter()
+        .map(|e| ((e.1.atan2(e.0) + PI) % (PI / 2.)).abs())
+        .collect();
+    edge_angles.dedup();
+
+    let mut min_area = std::f64::MAX;
+    let mut res = vec![(0., 0.); 4];
+    for angle in edge_angles {
+        let r = [[angle.cos(), -angle.sin()], [angle.sin(), angle.cos()]];
+
+        let rotated_points: Vec<(f64, f64)> = points
+            .iter()
+            .map(|p| {
+                (
+                    p.x.to_f64().unwrap() * r[0][0] + p.y.to_f64().unwrap() * r[1][0],
+                    p.x.to_f64().unwrap() * r[0][1] + p.y.to_f64().unwrap() * r[1][1],
+                )
+            })
+            .collect();
+        let (min_x, max_x, min_y, max_y) = rotated_points.iter().fold(
+            (std::f64::MAX, std::f64::MIN, std::f64::MAX, std::f64::MIN),
+            |acc, p| {
+                (
+                    acc.0.min(p.0),
+                    acc.1.max(p.0),
+                    acc.2.min(p.1),
+                    acc.3.max(p.1),
+                )
+            },
+        );
+        let width = max_x - min_x;
+        let height = max_y - min_y;
+        let area = width * height;
+        if area < min_area {
+            min_area = area;
+
+            res[0] = (
+                max_x * r[0][0] + min_y * r[0][1],
+                max_x * r[1][0] + min_y * r[1][1],
+            );
+            res[1] = (
+                min_x * r[0][0] + min_y * r[0][1],
+                min_x * r[1][0] + min_y * r[1][1],
+            );
+            res[2] = (
+                min_x * r[0][0] + max_y * r[0][1],
+                min_x * r[1][0] + max_y * r[1][1],
+            );
+            res[3] = (
+                max_x * r[0][0] + max_y * r[0][1],
+                max_x * r[1][0] + max_y * r[1][1],
+            );
+        }
+    }
+
+    res.sort_by(|a, b| {
+        if a.0 < b.0 {
+            std::cmp::Ordering::Less
+        } else if a.0 > b.0 {
+            std::cmp::Ordering::Greater
+        } else {
+            std::cmp::Ordering::Equal
+        }
+    });
+    let i1 = if res[1].1 > res[0].1 { 0 } else { 1 };
+    let i2 = if res[3].1 > res[2].1 { 2 } else { 3 };
+    let i3 = if res[3].1 > res[2].1 { 3 } else { 2 };
+    let i4 = if res[1].1 > res[0].1 { 1 } else { 0 };
+    vec![
+        Point::new(
+            cast(res[i1].0.floor()).unwrap(),
+            cast(res[i1].1.floor()).unwrap(),
+        ),
+        Point::new(
+            cast(res[i2].0.ceil()).unwrap(),
+            cast(res[i2].1.floor()).unwrap(),
+        ),
+        Point::new(
+            cast(res[i3].0.ceil()).unwrap(),
+            cast(res[i3].1.ceil()).unwrap(),
+        ),
+        Point::new(
+            cast(res[i4].0.floor()).unwrap(),
+            cast(res[i4].1.ceil()).unwrap(),
+        ),
+    ]
+}
+
+/// Finds points of the smallest convex polygon that contains all the contour points.
+/// Based on the [Graham scan algorithm].
+///
+/// [Graham scan algorithm]: https://en.wikipedia.org/wiki/Graham_scan
+fn convex_hull<T: Num + NumCast + Copy + PartialEq + Eq>(
+    points_slice: &[Point<T>],
+) -> Vec<Point<T>> {
+    let mut points: Vec<Point<usize>> = points_slice
+        .iter()
+        .map(|p| Point::new(p.x.to_usize().unwrap(), p.y.to_usize().unwrap()))
+        .collect();
+    let (start_point_pos, start_point) = points.iter().enumerate().fold(
+        (
+            std::usize::MAX,
+            Point::new(std::usize::MAX, std::usize::MAX),
+        ),
+        |(pos, acc_point), (i, &point)| {
+            if point.y < acc_point.y || point.y == acc_point.y && point.x < acc_point.x {
+                return (i, point);
+            }
+            (pos, acc_point)
+        },
+    );
+    points.swap(0, start_point_pos);
+    points.remove(0);
+    points.sort_by(|a, b| {
+        let orientation = get_orientation(&start_point, a, b);
+        if orientation == 0 {
+            if get_distance(&start_point, a) < get_distance(&start_point, b) {
+                return std::cmp::Ordering::Less;
+            }
+            return std::cmp::Ordering::Greater;
+        }
+        if orientation == 2 {
+            std::cmp::Ordering::Less
+        } else {
+            std::cmp::Ordering::Greater
+        }
+    });
+
+    let mut iter = points.iter().peekable();
+    let mut remaining_points = Vec::with_capacity(points.len());
+    while let Some(mut p) = iter.next() {
+        while iter.peek().is_some() && get_orientation(&start_point, p, iter.peek().unwrap()) == 0 {
+            p = iter.next().unwrap();
+        }
+        remaining_points.push(p);
+    }
+
+    let mut stack: Vec<Point<T>> = vec![Point::new(
+        cast(start_point.x).unwrap(),
+        cast(start_point.y).unwrap(),
+    )];
+
+    for p in points.iter() {
+        let point = Point::new(cast(p.x).unwrap(), cast(p.y).unwrap());
+        while stack.len() > 1
+            && get_orientation(&stack[stack.len() - 2], &stack[stack.len() - 1], &point) != 2
+        {
+            stack.pop();
+        }
+        stack.push(point);
+    }
+    stack
+}
+
+fn get_orientation<T: Num + NumCast + Copy + PartialEq + Eq>(
+    p: &Point<T>,
+    q: &Point<T>,
+    r: &Point<T>,
+) -> u8 {
+    let val = (q.y.to_i32().unwrap() - p.y.to_i32().unwrap())
+        * (r.x.to_i32().unwrap() - q.x.to_i32().unwrap())
+        - (q.x.to_i32().unwrap() - p.x.to_i32().unwrap())
+            * (r.y.to_i32().unwrap() - q.y.to_i32().unwrap());
+    match val.cmp(&0) {
+        Ordering::Equal => 0,   // colinear
+        Ordering::Greater => 1, // clockwise (right)
+        Ordering::Less => 2,    // counter-clockwise (left)
+    }
+}
+
+/// Calculates the distance between 2 points.
+///
+pub fn get_distance<T: Num + NumCast + Copy + PartialEq + Eq>(p1: &Point<T>, p2: &Point<T>) -> f64 {
+    ((p1.x.to_f64().unwrap() - p2.x.to_f64().unwrap()).powf(2.)
+        + (p1.y.to_f64().unwrap() - p2.y.to_f64().unwrap()).powf(2.))
+    .sqrt()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
-
+    use crate::definitions::Point;
     #[test]
     fn test_contours_structured() {
         use crate::drawing::draw_polygon_mut;
@@ -302,5 +609,108 @@ mod tests {
         assert!(contours[4].points.contains(&Point::new(290, 299)));
         assert_eq!(contours[4].is_outer, true);
         assert_eq!(contours[4].parent, None);
+    }
+
+    #[test]
+    fn line_params_test() {
+        let p1 = Point::new(5, 7);
+        let p2 = Point::new(10, 3);
+        assert_eq!(line_params(&p1, &p2), (4., 5., -55.));
+    }
+
+    #[test]
+    fn perpendicular_distance_test() {
+        let line_args = (8., 7., 5.);
+        let point = Point::new(2, 3);
+        assert!(perpendicular_distance(line_args, &point) - 3.9510276472 < 1e-10);
+    }
+
+    #[test]
+    fn get_contours_approx_points() {
+        use crate::drawing::draw_polygon_mut;
+        use image::{GrayImage, Luma};
+        let mut image = GrayImage::from_pixel(300, 300, Luma([0]));
+        let white = Luma([255]);
+
+        let star = vec![
+            Point::new(100, 20),
+            Point::new(120, 35),
+            Point::new(140, 30),
+            Point::new(115, 45),
+            Point::new(130, 60),
+            Point::new(100, 50),
+            Point::new(80, 55),
+            Point::new(90, 40),
+            Point::new(60, 25),
+            Point::new(90, 35),
+        ];
+        draw_polygon_mut(&mut image, &star, white);
+        let contours = find_contours::<u32>(&image);
+        let c1_approx = approx_poly_dp(
+            &contours[0].points,
+            arc_length(&contours[0].points, true) * 0.01,
+            true,
+        );
+        assert_eq!(
+            c1_approx,
+            vec![
+                Point::new(100, 20),
+                Point::new(90, 35),
+                Point::new(60, 25),
+                Point::new(90, 40),
+                Point::new(80, 55),
+                Point::new(101, 50),
+                Point::new(130, 60),
+                Point::new(115, 45),
+                Point::new(140, 30),
+                Point::new(120, 35)
+            ]
+        );
+    }
+
+    #[test]
+    fn get_convex_hull_points() {
+        let star = vec![
+            Point::new(100, 20),
+            Point::new(90, 35),
+            Point::new(60, 25),
+            Point::new(90, 40),
+            Point::new(80, 55),
+            Point::new(101, 50),
+            Point::new(130, 60),
+            Point::new(115, 45),
+            Point::new(140, 30),
+            Point::new(120, 35),
+        ];
+        let points = convex_hull(&star);
+        assert_eq!(
+            points,
+            [
+                Point::new(100, 20),
+                Point::new(140, 30),
+                Point::new(130, 60),
+                Point::new(80, 55),
+                Point::new(60, 25)
+            ]
+        );
+    }
+
+    #[test]
+    fn min_area_test() {
+        assert_eq!(
+            min_area_rect(&[
+                Point::new(100, 20),
+                Point::new(140, 30),
+                Point::new(130, 60),
+                Point::new(80, 55),
+                Point::new(60, 25)
+            ]),
+            [
+                Point::new(60, 16),
+                Point::new(141, 24),
+                Point::new(137, 61),
+                Point::new(57, 53)
+            ]
+        )
     }
 }
