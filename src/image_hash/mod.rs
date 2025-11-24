2dbee1f27acbdc26af369f64a0f6d45ea60a085c
Alex Severin <severinalexeyv@gmail.com>
2025-11-24 19:55:56 +0300

    add average image hash

--- src/image_hash/mod.rs
+++ src/image_hash/mod.rs
@@ -2,10 +2,12 @@
 //!
 //! [Perceptual hashing]: https://en.wikipedia.org/wiki/Perceptual_hashing
 
+mod average_hash;
 mod bits;
 mod phash;
 mod signals;
 
 use bits::Bits64;
 
+pub use average_hash::{average_hash, AverageHash};
 pub use phash::{phash, PHash};
