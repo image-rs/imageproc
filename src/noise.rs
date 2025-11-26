0a71b3589bf91fc397042201519a645c22b3f201
Dirleye <dirleyjordan@gmail.com>
2025-11-26 21:47:32 +0000

    Split speckle noise and speckle noise per channel into different functions

--- src/noise.rs
+++ src/noise.rs
@@ -34,55 +34,64 @@ where
 }
 
 /// Adds multiplicative speckle noise to an image with the given mean and standard deviation.
-/// Noise can be added independently per channel for data augmentation, or just per pixel for
-/// realistic sensor noise simulation.
-pub fn speckle_noise<P>(
-    image: &Image<P>,
-    per_channel: bool,
-    mean: f64,
-    stddev: f64,
-    seed: u64,
-) -> Image<P>
+/// Noise is added per pixel for realistic sensor noise simulation.
+pub fn speckle_noise<P>(image: &Image<P>, mean: f64, stddev: f64, seed: u64) -> Image<P>
 where
     P: Pixel,
     P::Subpixel: Into<f64> + Clamp<f64>,
 {
     let mut out = image.clone();
-    speckle_noise_mut(&mut out, per_channel, mean, stddev, seed);
+    speckle_noise_mut(&mut out, mean, stddev, seed);
     out
 }
+
 #[doc=generate_mut_doc_comment!("speckle_noise")]
-pub fn speckle_noise_mut<P>(
-    image: &mut Image<P>,
-    per_channel: bool,
-    mean: f64,
-    stddev: f64,
-    seed: u64,
-) where
+pub fn speckle_noise_mut<P>(image: &mut Image<P>, mean: f64, stddev: f64, seed: u64)
+where
     P: Pixel,
     P::Subpixel: Into<f64> + Clamp<f64>,
 {
     let mut rng: StdRng = SeedableRng::seed_from_u64(seed);
     let normal = Normal::new(mean, stddev).unwrap();
 
-    if per_channel {
-        // Add sampled noise per channel in each pixel
-        for p in image.pixels_mut() {
-            p.apply(|c| {
-                let noise = normal.sample(&mut rng);
-                let original = c.into();
-                P::Subpixel::clamp(original + original * noise)
-            });
-        }
-    } else {
-        // Use the same noise pattern for each channel
-        for p in image.pixels_mut() {
+    // Use the same noise pattern for each channel
+    for p in image.pixels_mut() {
+        let noise = normal.sample(&mut rng);
+        p.apply(|c| {
+            let original = c.into();
+            P::Subpixel::clamp(original + original * noise)
+        });
+    }
+}
+
+/// Adds multiplicative speckle noise to an image with the given mean and standard deviation.
+/// Noise is added independently per channel for data augmentation.
+pub fn speckle_noise_per_channel<P>(image: &Image<P>, mean: f64, stddev: f64, seed: u64) -> Image<P>
+where
+    P: Pixel,
+    P::Subpixel: Into<f64> + Clamp<f64>,
+{
+    let mut out = image.clone();
+    speckle_noise_per_channel_mut(&mut out, mean, stddev, seed);
+    out
+}
+
+#[doc=generate_mut_doc_comment!("speckle_noise_per_channel")]
+pub fn speckle_noise_per_channel_mut<P>(image: &mut Image<P>, mean: f64, stddev: f64, seed: u64)
+where
+    P: Pixel,
+    P::Subpixel: Into<f64> + Clamp<f64>,
+{
+    let mut rng: StdRng = SeedableRng::seed_from_u64(seed);
+    let normal = Normal::new(mean, stddev).unwrap();
+
+    // Add sampled noise per channel in each pixel
+    for p in image.pixels_mut() {
+        p.apply(|c| {
             let noise = normal.sample(&mut rng);
-            p.apply(|c| {
-                let original = c.into();
-                P::Subpixel::clamp(original + original * noise)
-            });
-        }
+            let original = c.into();
+            P::Subpixel::clamp(original + original * noise)
+        });
     }
 }
 
@@ -142,7 +151,16 @@ mod benches {
     fn bench_speckle_noise_mut(b: &mut Bencher) {
         let mut image = GrayImage::new(100, 100);
         b.iter(|| {
-            speckle_noise_mut(&mut image, true, 0.0, 0.4, 1);
+            speckle_noise_mut(&mut image, 0.0, 0.4, 1);
+        });
+        black_box(image);
+    }
+
+    #[bench]
+    fn bench_speckle_noise_per_channel_mut(b: &mut Bencher) {
+        let mut image = GrayImage::new(100, 100);
+        b.iter(|| {
+            speckle_noise_per_channel_mut(&mut image, 0.0, 0.4, 1);
         });
         black_box(image);
     }
