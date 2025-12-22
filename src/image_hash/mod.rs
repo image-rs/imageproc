cd971d648658e63c2ca1b21bef898e1a55a7e9e6
Your Name <quadekirby@gmail.com>
2025-12-22 08:00:25 -0500

    minor format fix

--- src/image_hash/mod.rs
+++ src/image_hash/mod.rs
@@ -3,15 +3,16 @@
 //! [Perceptual hashing]: https://en.wikipedia.org/wiki/Perceptual_hashing
 
 mod average_hash;
+#[cfg(feature = "fft")]
 mod bits;
 #[cfg(feature = "fft")]
 mod phash;
 #[cfg(feature = "fft")]
-#[cfg(feature = "fft")]
 mod signals;
+
+#[cfg(feature = "fft")]
+pub use phash::{phash, PHash};
 
 use bits::Bits64;
 
 pub use average_hash::{average_hash, AverageHash};
-#[cfg(feature = "fft")]
-pub use phash::{phash, PHash};
