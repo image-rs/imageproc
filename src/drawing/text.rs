b66d68d3363eb7bc6e6b622e64e76fb39bb322bb
var4yn <2016slavyakinnikita@gmail.com>
2026-03-26 17:26:02 +0800

    fix: case below ascent

--- src/drawing/text.rs
+++ src/drawing/text.rs
@@ -1,4 +1,5 @@
 use image::{GenericImage, Pixel};
+use num::Zero;
 use std::f32;
 
 use crate::definitions::{Clamp, Image};
@@ -21,20 +22,38 @@ fn layout_glyphs(
     let mut w = 0.0;
     let mut prev: Option<GlyphId> = None;
 
-    let mut h = 0.0_f32;
+    let mut y_max = 0.0_f32;
+    let mut y_min = 0.0_f32;
     let mut y_above_ascent = 0.0_f32;
+    let mut y_below_ascent = f32::MAX;
 
     // Find the peak `y_above_ascent` value which above ascent across all glyphs
+    // And the minimum `y_below_ascent` value which below ascent across all glyphs
     for c in text.chars() {
         let glyph_id = font.glyph_id(c);
         let glyph = glyph_id.with_scale_and_position(scale, point(w, font.ascent()));
         if let Some(g) = font.outline_glyph(glyph) {
             let bb = g.px_bounds();
+            
+            // for correct offset
             y_above_ascent = y_above_ascent.min(bb.min.y);
-            h = h.max(bb.max.y - bb.min.y);
+            y_below_ascent = y_below_ascent.min(bb.min.y).max(0.0);
+            
+            // for correct height
+            y_min = y_min.min(bb.min.y);
+            y_max = y_max.max(bb.max.y);
         }
     }
-    y_above_ascent = y_above_ascent.abs();
+
+    let offset_y = if y_above_ascent.is_zero() {
+        y_below_ascent
+    } else {
+        y_above_ascent
+    };
+
+    // height and remove overflow ascent
+    dbg!(y_above_ascent, y_below_ascent);
+    let h = y_max - y_min - y_below_ascent;
 
     for c in text.chars() {
         let glyph_id = font.glyph_id(c);
@@ -46,8 +65,8 @@ fn layout_glyphs(
             }
             prev = Some(glyph_id);
             let mut bb = g.px_bounds();
-            bb.min.y += y_above_ascent;
-            bb.max.y += y_above_ascent;
+            bb.min.y -= offset_y;
+            bb.max.y -= offset_y;
             f(g, bb);
         }
     }
