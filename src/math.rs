2672133897d66ba5ddfd09a794ff2691529665ab
cospectrum <severinalexeyv@gmail.com>
2026-03-30 23:55:08 +0300

    add math tests (#771)

--- src/math.rs
+++ src/math.rs
@@ -9,3 +9,21 @@ pub fn l1_norm(xs: &[f32]) -> f32 {
 pub fn l2_norm(xs: &[f32]) -> f32 {
     xs.iter().fold(0f32, |acc, x| acc + x * x).sqrt()
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_l1_norm() {
+        assert_eq!(l1_norm(&[]), 0.0);
+        assert_eq!(l1_norm(&[1.0, -2.0, 3.5]), 6.5);
+    }
+
+    #[test]
+    fn test_l2_norm() {
+        assert_eq!(l2_norm(&[]), 0.0);
+        assert_approx_eq!(l2_norm(&[3.0, 4.0]), 5.0);
+        assert_approx_eq!(l2_norm(&[1.0, -2.0, 2.0]), 3.0);
+    }
+}
